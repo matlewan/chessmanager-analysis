eb683a765eab3538fdb200230de358ed4608cb0d
matlewan <matlewan7@gmail.com>
2023-01-31 21:38:31 +0100

    Refactor

--- frontend/src/components/Range.vue
+++ frontend/src/components/Range.vue
@@ -1,26 +1,25 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const props = defineProps({ 
     modelValue: { type: Object, required: true }
 })
 
 const emit = defineEmits(['update:modelValue'])
-let min = ref(props.modelValue.min)
-let max = ref(props.modelValue.max)
+const min = ref(props.modelValue.min)
+const max = ref(props.modelValue.max)
 
-function handleInput (e) {
+watch([min, max], () => {
     let obj = { min: parseFloat(min.value), max: parseFloat(max.value) }
     emit('update:modelValue', obj)
-}
-
+})
 </script>
 
 <template>
     <div class="range">
-        <input type="number" v-model="min" @focusout="handleInput" />
+        <input type="number" v-model="min" />
         <label> - </label>
-        <input type="number" v-model="max" @focusout="handleInput" />
+        <input type="number" v-model="max" />
     </div>
 </template>
 
